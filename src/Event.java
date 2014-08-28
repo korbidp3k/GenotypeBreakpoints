063c9dbd5d533e8f4e67919df99dd35a321ef102
korbidp3k <adrianto@gmail.com>
2014-08-28 16:03:53 +0200

    Parsing for Delly Input

--- src/Event.java
+++ src/Event.java
@@ -69,9 +69,48 @@ public class Event {
 	 * 
 	 */
 	public static Event createNewEventFromDellyOutput(String output){
-		return null;
+		StringTokenizer t = new StringTokenizer(output, "\t:");
+		String chr1 = t.nextToken();
+		String chr2 = chr1;
+		int p1 = Integer.parseInt(t.nextToken());
+		int p2 = Integer.parseInt(t.nextToken());
+		t.nextToken();
+		t.nextToken();
+		t.nextToken();
+		String typeT;
+		String tempT = t.nextToken(); 
+		typeT = tempT.substring(1,tempT.indexOf("_"));
+		if (typeT.equals("Inversion")){
+			typeT = tempT.substring(1,(tempT.indexOf("_")+2));
+		}
+		
+		GenomicCoordinate c1 = new GenomicCoordinate(chr1, p1);
+		GenomicCoordinate c2 = new GenomicCoordinate(chr2, p2);
+		EVENT_TYPE type = classifyDellyBreakpoint(c1, c2, typeT);
+		
+		System.out.println(chr1 +"\t"+ p1 +"\t"+ p2 +"\t" + type +"\t"+ typeT);
+		
+		return new Event(c1, c2, type);
 	}
 
+	/*
+	 * Function to classify a line of Delly output into a genomic event type.
+	 * The distinctions between INV1/2 etc are arbitrary, and have to be consistent across all the inputs.
+	 */
+	private static EVENT_TYPE classifyDellyBreakpoint(GenomicCoordinate c1, GenomicCoordinate c2, String t){
+		if(t.equals("Inversion_0")){
+			return EVENT_TYPE.INV1;
+		} else if (t.equals("Inversion_1")){
+			return EVENT_TYPE.INV2;
+		} else if (t.equals("Deletion")){
+			return EVENT_TYPE.DEL;
+		} else if (t.equals("Duplication")){
+			return EVENT_TYPE.TAN;
+		} else {
+			return EVENT_TYPE.XXX;
+		}
+	}
+	
 	public GenomicCoordinate getC1() {
 		return c1;
 	}
